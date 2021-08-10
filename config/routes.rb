0568f2b8e5fcb48d5a20696e317199e7df4ed956
cheyenne simoleit <cheyenne@simoleit.com>
2021-08-10 15:45:47 +0200

    First rails ting done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # List all tasks
+  get "tasks", to: "tasks#index"
+  
+  # Get the create task form
+  get "tasks/new", to: "tasks#new"
+
+  # show individual task details
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # send the new task to be saved
+  post "tasks", to: "tasks#create"
+
+  # Make new from with details already in to update
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+
+  # Save the updated tasks
+  patch "tasks/:id", to: "tasks#update"
+
+  # Destroy tasks
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
+
 end
